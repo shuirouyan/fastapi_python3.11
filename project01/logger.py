4f24cc023d50b6cd4e0529b289fd7e6af05f3916
shuirouyan <1141761757@qq.com>
2026-01-07 17:35:06 +0800

    logger format, scalars and mappings

--- project01/logger.py
+++ project01/logger.py
@@ -1,6 +1,10 @@
-
 import logging
 
-logging.basicConfig(level=logging.INFO)
+
+# 配置统一日志格式
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT, datefmt=DATE_FORMAT)
 
 logger = logging.getLogger(__name__)
--- project01/route/route3.py
+++ project01/route/route3.py
@@ -9,11 +9,15 @@ from logger import logger
 from fastapi.responses import JSONResponse, RedirectResponse
 import json
 
-route3 = APIRouter(prefix="/v3", tags=["route3使用JSONResponse"])
+# route3使用JSONResponse
+route3 = APIRouter(prefix="/v3", tags=["V3"])
 
 
-@route3.get("/list", tags=["从数据看获取usersmscode"])
+@route3.get("/list")
 async def get_users(db: AsyncSession = Depends(get_db)) -> JSONResponse:
+    """
+    从数据看获取usersmscode
+    """
     stm = select(UserSmsCode.id, UserSmsCode.sms_code).order_by(UserSmsCode.id.desc())
     result = await db.execute(statement=stm)
     resp = result.mappings().fetchall()
@@ -23,12 +27,25 @@ async def get_users(db: AsyncSession = Depends(get_db)) -> JSONResponse:
     )
 
 
-@route3.post("/redirect", tags=["307重定向"])
+@route3.get("/list_fieldall")
+async def get_users_field_all(db: AsyncSession = Depends(get_db)) -> JSONResponse:
+    """从数据看获取usersmscode all field"""
+    stm = select(UserSmsCode).order_by(UserSmsCode.id.desc())
+    result = await db.execute(statement=stm)
+    resp = result.scalars().fetchall()
+    logger.info("resp:{},type:{}".format(resp, type(resp)))
+    return JSONResponse(
+        content=[item.to_dict() for item in resp], status_code=status.HTTP_200_OK
+    )
+
+
+@route3.post("/redirect")
 async def redirect_method(
     request: Request,
     param1: str = Query(default="参数1"),
     param2: str = Body(default="http://www.baidu.com", embed=True),
 ) -> RedirectResponse:
+    """307重定向"""
     logger.info("param1:{},param2:{}".format(param1, param2))
     headers = request.headers
     # headers["param1"] = param1
