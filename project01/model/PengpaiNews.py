b45881173d430974a26509f9efe8613c7872de61
shuirouyan <1141761757@qq.com>
2026-03-03 11:04:30 +0800

    feat: 添加澎湃新闻数据存储功能
    新增PengpaiNews模型类并实现数据保存接口

---
+++ project01/model/PengpaiNews.py
@@ -0,0 +1,26 @@
+from sqlalchemy.orm import DeclarativeBase
+
+from sqlalchemy.orm import Mapped, mapped_column as Column
+from sqlalchemy import Integer, String, DateTime, BIGINT, TEXT
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class PengpaiNews(Base):
+    """
+    CREATE TABLE `pengpai_news` (
+    `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `title` varchar(255) DEFAULT NULL COMMENT 'title',
+    `conent_msg` text COMMENT '内容',
+    `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    """
+
+    __tablename__ = "pengpai_news"
+    id = Column(BIGINT, primary_key=True, index=True)
+    title = Column(String(255), nullable=True)
+    conent_msg = Column(TEXT)
+    create_time = Column(DateTime, nullable=True, default=DateTime)
--- project01/route/route5.py
+++ project01/route/route5.py
@@ -6,9 +6,10 @@ import json
 from database import get_db
 import datetime
 from redis.asyncio import Redis
+from sqlalchemy import insert
+from model.PengpaiNews import PengpaiNews
 
 route5 = APIRouter(prefix="/v5", tags=["V5"])
-
 
 
 @route5.post("/save", tags=["澎湃新闻"])
@@ -21,12 +22,13 @@ async def pengpai_info_msg(
     body = await request.body()
     body_str = body.decode("utf-8")
     body_json = json.loads(body_str)
-    logger.info(f"body:{body_json}")
+    logger.info(f"body:{body_json}, id:{id}, name:{name}")
     if body != None and body_str.strip() != "":
         # await redis.setnx(f"pengpai:{name}", body_json)
         # 保存到数据库
-        # await db.execute()
-        pass
+        stm = insert(PengpaiNews).values(title=name, conent_msg=body_str)
+        result_row = await db.execute(stm)
+        logger.info(f"result_row:{result_row}")
     else:
         logger.info("Empty body received, id:{id}")
     return {
