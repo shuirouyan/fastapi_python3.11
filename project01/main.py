580225c55794f51149c3212bbbbe53d0cdc60223
shuirouyan <1141761757@qq.com>
2026-03-23 18:45:53 +0800

    fix: 修改应用监听端口为8002

--- project01/main.py
+++ project01/main.py
@@ -187,4 +187,4 @@ async def user_sms_code_method(
 
 if __name__ == "__main__":
     logger.info("Starting the FastAPI application")
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=8002, reload=True, log_level="info")
